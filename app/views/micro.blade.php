605ed2872214549aa803619c3a61ef7190aa324c
Robert Merki <robmerki@gmail.com>
2014-09-01 15:38:01 -0700

    made micro site

---
+++ app/views/micro.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>nwHacks 2015</title>
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" type="image/png" href="favicon.png">
+	<meta
+		name="viewport"
+		content="
+      width=device-width,
+      initial-scale=1,
+      minimum-scale=1,
+      maximum-scale=1
+    "
+		/>
+</head>
+
+<body>
+<header id="micro-header">
+		<div class="darkness"></div>
+		<div id="micro-hero">
+				<h2>Small but</h2><h3>EPIC</h3>
+		</div>
+		<div class="logo-left"></div>
+		<div class="triangle-left" >
+			<img class="triangle" src="img/micro/triangle-left.png" alt="">
+			<img class="logo" src="img/micro/nwhacks_micro_logo_white.png" alt="">
+		</div>
+		<div class="triangle-right"><img  src="img/micro/triangle-right.png" alt=""></div>
+</header>
+
+<section id="micro-intro">
+
+	<article class="grid-container">
+		<div class="grid-100 logo"><img src="img/micro/logo-micro-opaque.png" alt=""></div>
+		<div class="date grid-100">
+			<time datetime="2015-01-09">
+				<span class="month">September</span>
+				<span class="day">20<sup>th</sup></span>
+				<span class="year">2014</span>
+			</time>
+		</div>
+		<p class="grid-100">
+			nwHacks Micro is the first big hacker event of the year. We're putting together an eight hour event for everyone to get a taste of what the full nwHacks experience will be like. Come out, make friends, and hack together something extraordinary. 
+			<br>
+			Membership to StartupStorm is mandatory.
+		</p>
+		<form class="prefix-33 suffix-33 grid-33 tablet-grid-60 tablet-prefix-20 tablet-suffix-20" action="">
+			<label>Personal</label>
+			<input type="text" placeholder="First Name">
+			<input type="text" placeholder="Last Name">
+			<input type="text" placeholder="Email">
+
+			<label>Professional</label>
+			<input type="text" placeholder="School">
+			<input type="text" placeholder="GitHub">
+			<input type="text" placeholder="LinkedIn">
+
+			<label>Your work</label>
+			<textarea class="experience" placeholder="Experience"></textarea>
+			<input type="submit" value="Submit">
+		</form>
+	</article>
+	<div class="grey-triangle-right"><img src="img/micro/grey-triangle-right.png" alt=""></div>
+	<div class="grey-triangle-left"><img src="img/micro/grey-triangle-left.png" alt=""></div>
+</section>
+
+<footer class="grid-parent">
+	<nav class="grid-container">
+		<!--<a href="#">Register</a>--> <a href="mailto:david@nwhacks.com">Become a Sponsor</a> <a href="mailto:david@nwhacks.com">Email us</a>
+	</nav>
+	<p class="grid-container">nwHacks Micro 2014 - Organised and held by <a href="https://www.startupstorm.org/" target="_blank">StartupStorm</a>
+	</p>
+
+	<div class="imgcont"><a href="http://www.nwhacks.com"><img class src="img/logotxt.png" alt=""></a></div>
+
+</footer>
+
+<script src="js/min/scripts.min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53890324-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+</html>
